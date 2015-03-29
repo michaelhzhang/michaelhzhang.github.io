746a50b78bf0c015147a532e7d2d4c6fbdc88fb5
Michael Zhang <michael.zhang@berkeley.edu>
2015-03-29 15:17:05 -0700

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,122 @@
+function homie() {
+	$( '.projects' ).fadeOut(300);
+	$(".home").fadeIn(300);
+}
+
+function projects() {
+	$(".home").fadeOut(300);
+	$( '.projects' ).fadeIn(300);
+}
+
+function blog() {
+	$(".home").fadeOut(300);
+	$( '.projects' ).fadeOut(300);
+}
+
+function page0() {
+	$('.page0').fadeIn(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page1() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeIn(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page2() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeIn(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page3() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeIn(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page4() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeIn(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page5() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeIn(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page6() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeIn(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeOut(300);
+}
+
+function page7() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeIn(300);
+	$('.page8').fadeOut(300);
+}
+
+function page8() {
+	$('.page0').fadeOut(300);
+	$('.page1').fadeOut(300);
+	$('.page2').fadeOut(300);
+	$('.page3').fadeOut(300);
+	$('.page4').fadeOut(300);
+	$('.page5').fadeOut(300);
+	$('.page6').fadeOut(300);
+	$('.page7').fadeOut(300);
+	$('.page8').fadeIn(300);
+}
